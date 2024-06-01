29a74da212d532ab43677ceb8cbdf46f580c78a2
Rafey <rafeldo36@gmail.com>
2024-06-01 17:21:43 +0530

    first commit

---
+++ mongodb/connect.js
@@ -0,0 +1,12 @@
+import mongoose, { mongo } from "mongoose";
+
+const connectDB = (uri) => {
+  mongoose.set("strictQuery", true);
+
+  mongoose
+    .connect(uri)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
+};
+
+export default connectDB;
